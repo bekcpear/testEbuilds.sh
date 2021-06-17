0a54b40696aa802a0cc1177fef7b1ba8aaa8e7e5
bekcpear <i@bitbili.net>
2021-06-18 03:30:54 +0800

    update tool

--- .tool/cleanAll.sh
+++ .tool/cleanAll.sh
@@ -9,33 +9,33 @@ SRCPATH=$(realpath "${0}")
 SRCPATH=${SRCPATH%/.tool/*}
 . ${SRCPATH}/_log.sh
 
-_log n ">> umount -fv ${BASE_DIR}/_testEbuilds-*/**/TMPFS"
-if ls ${BASE_DIR}/_testEbuilds-*/**/TMPFS &>/dev/null; then
-  umount -fv ${BASE_DIR}/_testEbuilds-*/**/TMPFS
+_log n ">> umount -fv ${BASE_DIR}/_testEbuilds:*/**/TMPFS"
+if ls ${BASE_DIR}/_testEbuilds:*/**/TMPFS &>/dev/null; then
+  umount -fv ${BASE_DIR}/_testEbuilds:*/**/TMPFS
 else
   echo "<NONE>"
 fi
 
 echo
-_log n ">> btrfs subvolume delete ${BASE_DIR}/_testEbuilds-*/**/SNAPSHOT"
-if ls ${BASE_DIR}/_testEbuilds-*/**/SNAPSHOT &>/dev/null; then
-  btrfs subvolume delete ${BASE_DIR}/_testEbuilds-*/**/SNAPSHOT
+_log n ">> btrfs subvolume delete ${BASE_DIR}/_testEbuilds:*/**/SNAPSHOT"
+if ls ${BASE_DIR}/_testEbuilds:*/**/SNAPSHOT &>/dev/null; then
+  btrfs subvolume delete ${BASE_DIR}/_testEbuilds:*/**/SNAPSHOT
 else
   echo "<NONE>"
 fi
 
 echo
-_log n ">> rm -rfv ${BASE_DIR}/_testEbuilds-*"
-if ls ${BASE_DIR}/_testEbuilds-* &>/dev/null; then
-  rm -rfv ${BASE_DIR}/_testEbuilds-*
+_log n ">> rm -rfv ${BASE_DIR}/_testEbuilds:*"
+if ls ${BASE_DIR}/_testEbuilds:* &>/dev/null; then
+  rm -rfv ${BASE_DIR}/_testEbuilds:*
 else
   echo "<NONE>"
 fi
 
 echo
-_log n ">> rm -v /tmp/_testEbuilds-*"
-if ls /tmp/_testEbuilds-* &>/dev/null; then
-  rm -v /tmp/_testEbuilds-*
+_log n ">> rm -v /tmp/_testEbuilds:*"
+if ls /tmp/_testEbuilds:* &>/dev/null; then
+  rm -v /tmp/_testEbuilds:*
 else
   echo "<NONE>"
 fi
