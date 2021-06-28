a2e883079ad1f0579b44014289aed1902e51bed4
bekcpear <i@bitbili.net>
2021-06-28 19:57:16 +0800

    update tool

--- .tool/updateBaseSnapshots.sh
+++ .tool/updateBaseSnapshots.sh
@@ -19,12 +19,13 @@ for snapshot in ${SNAPSHOTS[@]}; do
     _log n "udpating ${path} ..."
     echo
     bwrap --bind "${path}" / \
+      --bind /var/cache/distfiles /var/cache/distfiles \
       --ro-bind /etc/resolv.conf /etc/resolv.conf \
       --ro-bind ${GENTOOBASE} /var/db/repos/gentoo \
       --dev /dev \
       --proc /proc \
       --tmpfs /dev/shm \
       --tmpfs /var/tmp \
-      /bin/bash --login -c 'chmod 1777 /dev/shm; emerge -jvuDN @world; emerge -c'
+      /bin/bash --login -c 'chmod 1777 /dev/shm; MAKEOPTS="-j30" emerge -jvuDN @world && emerge -c'
   done <<<$(ls -1d ${snapshot})
 done
